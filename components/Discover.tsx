b64ba58fe7db97973809c50df3da9240062a85c4
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2022-12-03 20:56:58 +0530

    Added Chips Component with Props. Interface added in component

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Chips from './DesignComponents/Chips'
+import { topics } from '../static/constants'
 
 const Discover = () => {
   return (
-    <div>Discover</div>
+    <div className="flex flex-wrap gap-2 space-y-2 max-w-xs">
+        {
+            topics.map(item => (
+                <Chips icon={item.icon} displayText={item.name}/>
+            ))
+        }
+    </div>
   )
 }
 
