6da61a5ab3903da086f10600f098df8c2cd0d41f
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2022-12-03 19:09:33 +0530

    Added Sidebar Layout

---
+++ components/Icons/CrossIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CrossIcon = () => {
+  return (
+    <div><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7 7L17 17" stroke="#2B3340" stroke-width="2" stroke-linecap="round"/>
+    <path d="M17 6.99998L6.99999 17" stroke="#2B3340" stroke-width="2" stroke-linecap="round"/>
+    </svg>
+    </div>
+  )
+}
+
+export default CrossIcon
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+//Icons
+import MenuIcon from './Icons/MenuIcon';
+import CrossIcon from './Icons/CrossIcon';
+import HomeIcon from './Icons/HomeIcon';
+
+// Components
+import Discover from './Discover';
+import Suggestions from './Suggestions';
+import Footer from './Footer';
+
+// Animation
+import 'animate.css';
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
+    const [showSidebar, setShowSidebar] = useState(false);
+    return (
+        <div className="h-full border-r border-gray-200">
+            <div 
+                className='block xl:hidden m-2 ml-4 mt-3 text-xl'
+                onClick={() => setShowSidebar((prev) => !prev)}
+            >
+                {
+                    showSidebar ? <MenuIcon/> : <CrossIcon />
+                }
+            </div>
+            <div className="flex flex-col h-full px-4 py-3 space-y-2">
+                <HomeIcon />
+                <Discover />
+                <Suggestions />
+                <Footer />
+            </div>
+        </div>
+    )
 }
 
 export default Sidebar
